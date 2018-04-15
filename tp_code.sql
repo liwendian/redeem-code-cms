f84fae8524cb1c8c354282b274331c61069d523d
LiWendian <wendianli@outlook.com>
2018-04-15 12:03:50 +0800

    数据库

---
+++ tp_code.sql
@@ -0,0 +1,4 @@
+INSERT INTO `tp_code`(`id`, `oid`, `uname`, `code`, `checknum`, `otime`, `class`) VALUES (4, '12345678', 'mia', '876554332', 10, '2017-11-31 19:33:33', '类型二');
+INSERT INTO `tp_code`(`id`, `oid`, `uname`, `code`, `checknum`, `otime`, `class`) VALUES (3, '11111113', 'bbb', 'fasdfasdfafs55', 1, '2017-1-02 18:33:33', '类型一');
+INSERT INTO `tp_code`(`id`, `oid`, `uname`, `code`, `checknum`, `otime`, `class`) VALUES (6, '778899', 'mia', '54651435435', 0, '2018-2-12 19:33:33', '类型二');
+INSERT INTO `tp_code`(`id`, `oid`, `uname`, `code`, `checknum`, `otime`, `class`) VALUES (7, 'f5gf4d5g', 'mia', '5454fd5sd55g', 0, '2014-11-31 19:33:33', '类型三');
